d6d68c99d8635e328b4d610dc00cd4363cfe1203
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-11-11 18:59:20 +0530

    kuntao: Fix init build
    Change-Id: I23def1cb241a62e792fe8bd7bfbead50ebbd2f60

--- init/Android.mk
+++ init/Android.mk
@@ -3,11 +3,10 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := \
-    system/core/base/include \
-    system/core/init
-LOCAL_CPPFLAGS := -Wall
-LOCAL_SRC_FILES := init_kuntao.cpp
+LOCAL_C_INCLUDES := system/core/init
 LOCAL_MODULE := libinit_kuntao
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := init_kuntao.cpp
+LOCAL_STATIC_LIBRARIES := libbase
 
 include $(BUILD_STATIC_LIBRARY)
